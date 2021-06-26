7cc3bfbc9533ecfd60c7bddca2efb3cbec579841
DerrickXuNu <derrickxu1994@gmail.com>
2021-06-25 21:36:12 -0700

    initial commit for customization

---
+++ opencda/customize/core/sensing/localization/localization_manager.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Customized Localization Module.
+"""
+# Author: Runsheng Xu <rxx3386@ucla.edu>
+# License: MIT
+
+from opencda.core.sensing.localization.localization_manager import LocalizationManager
+
+class CustomizedLocalizationManager()
--- test/test_ekf.py
+++ test/test_ekf.py
@@ -9,15 +9,12 @@ import os
 import sys
 import unittest
 
-import numpy as np
-
 # temporary solution for relative imports in case opencda is not installed
 # if opencda is installed, no need to use the following line
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 
-import mocked_carla as mcarla
-from opencda.core.sensing.localization.extented_kalman_filter import ExtentedKalmanFilter
+from opencda.customize.core.sensing.localization.extented_kalman_filter import ExtentedKalmanFilter
 
 
 class testKalmanFilter(unittest.TestCase):
