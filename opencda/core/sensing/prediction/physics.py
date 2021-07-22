7644b74fc0436756deb0fb5e10de937bc0dabf06
XHwind <310381979@qq.com>
2021-07-22 16:04:09 -0700

    update physics model

--- opencda/core/sensing/prediction/physics.py
+++ opencda/core/sensing/prediction/physics.py
@@ -3,27 +3,48 @@
 Physcis-based trajectory prediction model
 """
 
-
 # Author: CARLA Team, Hao Xiang <haxiang@g.ucla.edu>
 # License: MIT
 
 import numpy as np
 from collections import deque
 
+
+def angle_diff(x, y):
+    """
+    Get the smallest angle difference between 2 angles: the angle from y to x.
+    Parameters
+    ----------
+    x : float
+        To angle
+
+    y : float
+        From angle
+
+    Returns
+    -------
+    diff : float
+        Angle difference from y to x.
+
+    """
+    # calculate angle difference, modulo to [0, 2*pi]
+    period = 2 * np.pi
+    diff = (x - y + period / 2) % period - period / 2
+    if diff > np.pi:
+        diff = diff - (2 * np.pi)  # shift (pi, 2*pi] to (-pi, 0]
+    return diff
+
+
 class TrajectoryData:
     def __init__(self, observed_length):
         self.observed_traj = deque(maxlen=observed_length)
         self.observed_velocity = deque(maxlen=observed_length)
         self.observed_yaw = deque(maxlen=observed_length)
-        self.observed_yaw_rate = deque(maxlen=observed_length)
-    def add(self, pose, v, yaw, yaw_rate):
+
+    def add(self, pose, v, yaw):
         self.observed_traj.add(pose)
         self.observed_velocity.add(v)
         self.observed_yaw.add(yaw)
-        self.observed_yaw_rate(yaw_rate)
-
-
-
 
 
 class Baseline:
@@ -52,14 +73,15 @@ class Baseline:
         self.dt = dt
 
 class ConstantVelocityHeading(Baseline):
-    def __call__(self, observed_traj, observed_velocity, yaw):
-        x = observed_velocity[-1][0]
-        y = observed_velocity[-1][1]
-        v = observed_velocity[-1]
+    def __call__(self, observed_traj, v, yaw):
+        x = observed_traj[-1][0]
+        y = observed_traj[-1][1]
+        vx, vy = v * np.cos(yaw), v * np.sin(yaw)
+        pred_traj = constant_velocity_heading(x, y, vx, vy, self.predict_length, self.dt)
+        return pred_traj
 
 
-
-def constant_velocity_heading(self, x, y, vx, vy, predict_length, dt):
+def constant_velocity_heading(x, y, vx, vy, predict_length, dt):
     """
     Predict trajectories based on constant velocity.
     Parameters
@@ -84,12 +106,13 @@ def constant_velocity_heading(self, x, y, vx, vy, predict_length, dt):
 
     Returns
     -------
+    preds : np.array
+        Predicted trajectory with shape (predict_length, 2)
 
     """
     preds = []
-    for i in range(1, predict_length+1):
+    for i in range(1, predict_length + 1):
         t = i * dt
         preds.append((x + vx * t, y + vy * t))
     return np.array(preds)
-
 
