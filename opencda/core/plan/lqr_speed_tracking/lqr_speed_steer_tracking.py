ab891d25b350c7ac723faa7bdb40082ca8b0061a
Zhaoliang <zhz03@g.ucla.edu>
2022-05-18 15:11:54 -0700

    Upload: lqr speed steer tracking

---
+++ opencda/core/plan/lqr_speed_tracking/lqr_speed_steer_tracking.py
@@ -0,0 +1,464 @@
+import sys
+from opencda.core.plan import spline
+import scipy.linalg as la
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+"""
+Reference:
+Path tracking simulation with LQR speed and steering control
+author Atsushi Sakai (@Atsushi_twi)
+"""
+
+# Vehicle parameters
+LENGTH = 4.5  # [m]
+WIDTH = 2.0  # [m]
+BACKTOWHEEL = 1.0  # [m]
+WHEEL_LEN = 0.3  # [m]
+WHEEL_WIDTH = 0.2  # [m]
+TREAD = 0.7  # [m]
+WB = 2.5  # [m]
+
+MAX_STEER = np.deg2rad(45.0)  # maximum steering angle [rad]
+MAX_DSTEER = np.deg2rad(30.0)  # maximum steering speed [rad/s]
+MAX_SPEED = 55.0 / 3.6  # maximum speed [m/s]
+MIN_SPEED = -20.0 / 3.6  # minimum speed [m/s]
+MAX_ACCEL = 1.0  # maximum accel [m/ss]
+
+show_animation = True
+
+
+# LQR parameter
+Q = np.eye(5)
+R = np.eye(2)
+
+# parameters
+dt = 0.1  # time tick[s]
+L = 0.5  # Wheel base of the vehicle [m]
+max_steer = np.deg2rad(45.0)  # maximum steering angle[rad]
+
+show_animation = True
+
+
+class State:
+
+    def __init__(self, x=0.0, y=0.0, yaw=0.0, v=0.0):
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+        self.v = v
+
+
+def update(state, a, delta):
+
+    if delta >= max_steer:
+        delta = max_steer
+    if delta <= - max_steer:
+        delta = - max_steer
+
+    state.x = state.x + state.v * math.cos(state.yaw) * dt
+    state.y = state.y + state.v * math.sin(state.yaw) * dt
+    state.yaw = state.yaw + state.v / L * math.tan(delta) * dt
+    state.v = state.v + a * dt
+
+    return state
+
+
+def pi_2_pi(angle):
+    return (angle + math.pi) % (2 * math.pi) - math.pi
+
+
+def solve_DARE(A, B, Q, R):
+    """
+    solve a discrete time_Algebraic Riccati equation (DARE)
+    """
+    X = Q
+    maxiter = 150
+    eps = 0.01
+
+    for i in range(maxiter):
+        Xn = A.T @ X @ A - A.T @ X @ B @ \
+            la.inv(R + B.T @ X @ B) @ B.T @ X @ A + Q
+        if (abs(Xn - X)).max() < eps:
+            break
+        X = Xn
+
+    return Xn
+
+
+def dlqr(A, B, Q, R):
+    """Solve the discrete time lqr controller.
+    x[k+1] = A x[k] + B u[k]
+    cost = sum x[k].T*Q*x[k] + u[k].T*R*u[k]
+    # ref Bertsekas, p.151
+    """
+
+    # first, try to solve the ricatti equation
+    X = solve_DARE(A, B, Q, R)
+
+    # compute the LQR gain
+    K = la.inv(B.T @ X @ B + R) @ (B.T @ X @ A)
+
+    eigVals, eigVecs = la.eig(A - B @ K)
+
+    return K, X, eigVals
+
+
+def lqr_steering_control(state, cx, cy, cyaw, ck, pe, pth_e, sp):
+    ind, e = calc_nearest_index(state, cx, cy, cyaw)
+
+    tv = sp[ind]
+
+    k = ck[ind]
+    v = state.v
+    th_e = pi_2_pi(state.yaw - cyaw[ind])
+
+    A = np.zeros((5, 5))
+    A[0, 0] = 1.0
+    A[0, 1] = dt
+    A[1, 2] = v
+    A[2, 2] = 1.0
+    A[2, 3] = dt
+    A[4, 4] = 1.0
+    # print(A)
+
+    B = np.zeros((5, 2))
+    B[3, 0] = v / L
+    B[4, 1] = dt
+
+    K, _, _ = dlqr(A, B, Q, R)
+
+    x = np.zeros((5, 1))
+
+    x[0, 0] = e
+    x[1, 0] = (e - pe) / dt
+    x[2, 0] = th_e
+    x[3, 0] = (th_e - pth_e) / dt
+    x[4, 0] = v - tv
+
+    ustar = -K @ x
+
+    # calc steering input
+
+    ff = math.atan2(L * k, 1)
+    fb = pi_2_pi(ustar[0, 0])
+
+    # calc accel input
+    ai = ustar[1, 0]
+
+    delta = ff + fb
+
+    return delta, ind, e, th_e, ai
+
+
+def calc_nearest_index(state, cx, cy, cyaw):
+    dx = [state.x - icx for icx in cx]
+    dy = [state.y - icy for icy in cy]
+
+    d = [idx ** 2 + idy ** 2 for (idx, idy) in zip(dx, dy)]
+
+    mind = min(d)
+
+    ind = d.index(mind)
+
+    mind = math.sqrt(mind)
+
+    dxl = cx[ind] - state.x
+    dyl = cy[ind] - state.y
+
+    angle = pi_2_pi(cyaw[ind] - math.atan2(dyl, dxl))
+    if angle < 0:
+        mind *= -1
+
+    return ind, mind
+
+
+def closed_loop_prediction(cx, cy, cyaw, ck, speed_profile, goal):
+    T = 500.0  # max simulation time
+    goal_dis = 0.3
+    stop_speed = 0.05
+
+    state = State(x=-0.0, y=-0.0, yaw=0.0, v=0.0)
+
+    time = 0.0
+    x = [state.x]
+    y = [state.y]
+    yaw = [state.yaw]
+    v = [state.v]
+    t = [0.0]
+    acc = [0.0]
+
+    e, e_th = 0.0, 0.0
+
+    while T >= time:
+        dl, target_ind, e, e_th, ai = lqr_steering_control(
+            state, cx, cy, cyaw, ck, e, e_th, speed_profile)
+        """
+        if ai >= 1:
+            ai = 1
+        elif ai <= -1:
+            ai = -1
+        """
+        state = update(state, ai, dl)
+
+        if abs(state.v) <= stop_speed:
+            target_ind += 1
+
+        time = time + dt
+
+        # check goal
+        dx = state.x - goal[0]
+        dy = state.y - goal[1]
+        if math.sqrt(dx ** 2 + dy ** 2) <= goal_dis:
+            print("Goal")
+            break
+
+        x.append(state.x)
+        y.append(state.y)
+        yaw.append(state.yaw)
+        v.append(state.v)
+        t.append(time)
+        acc.append(ai)
+
+        if target_ind % 1 == 0 and show_animation:
+            plt.cla()
+            plt.plot(cx, cy, "-r", label="course")
+            plt.plot(x, y, "ob", label="trajectory")
+            plot_car(cx[target_ind], cy[target_ind], cyaw[target_ind])
+            plt.plot(cx[target_ind], cy[target_ind], "xg", label="target")
+            # print("target_ind",target_ind)
+            plt.axis("equal")
+            # plt.grid(True)
+            plt.title("speed[km/h]:" + str(round(state.v * 3.6, 2))
+                      + ",target index:" + str(target_ind))
+            plt.pause(0.0001)
+
+    return t, x, y, yaw, v,acc
+
+
+def calc_speed_profile(cx, cy, cyaw, target_speed):
+    speed_profile = [target_speed] * len(cx)
+
+    direction = 1.0
+
+    # Set stop point
+    for i in range(len(cx) - 1):
+        dyaw = abs(cyaw[i + 1] - cyaw[i])
+        switch = math.pi / 4.0 <= dyaw < math.pi / 2.0
+
+        if switch:
+            direction *= -1
+
+        if direction != 1.0:
+            speed_profile[i] = - target_speed
+        else:
+            speed_profile[i] = target_speed
+
+        if switch:
+            speed_profile[i] = 0.0
+
+    # speed down
+    for i in range(40):
+        speed_profile[-i] = target_speed / (50 - i)
+        if speed_profile[-i] <= 1.0 / 3.6:
+            speed_profile[-i] = 1.0 / 3.6
+
+    return speed_profile
+
+def plot_car(x, y, yaw, steer=0.0, cabcolor="-r", truckcolor="-k"):  # pragma: no cover
+ 
+    outline = np.array([[-BACKTOWHEEL, (LENGTH - BACKTOWHEEL), (LENGTH - BACKTOWHEEL), -BACKTOWHEEL, -BACKTOWHEEL],
+                        [WIDTH / 2, WIDTH / 2, - WIDTH / 2, -WIDTH / 2, WIDTH / 2]])
+
+    fr_wheel = np.array([[WHEEL_LEN, -WHEEL_LEN, -WHEEL_LEN, WHEEL_LEN, WHEEL_LEN],
+                         [-WHEEL_WIDTH - TREAD, -WHEEL_WIDTH - TREAD, WHEEL_WIDTH - TREAD, WHEEL_WIDTH - TREAD, -WHEEL_WIDTH - TREAD]])
+
+    rr_wheel = np.copy(fr_wheel)
+
+    fl_wheel = np.copy(fr_wheel)
+    fl_wheel[1, :] *= -1
+    rl_wheel = np.copy(rr_wheel)
+    rl_wheel[1, :] *= -1
+
+    Rot1 = np.array([[math.cos(yaw), math.sin(yaw)],
+                     [-math.sin(yaw), math.cos(yaw)]])
+    Rot2 = np.array([[math.cos(steer), math.sin(steer)],
+                     [-math.sin(steer), math.cos(steer)]])
+
+    fr_wheel = (fr_wheel.T.dot(Rot2)).T
+    fl_wheel = (fl_wheel.T.dot(Rot2)).T
+    fr_wheel[0, :] += WB
+    fl_wheel[0, :] += WB
+
+    fr_wheel = (fr_wheel.T.dot(Rot1)).T
+    fl_wheel = (fl_wheel.T.dot(Rot1)).T
+
+    outline = (outline.T.dot(Rot1)).T
+    rr_wheel = (rr_wheel.T.dot(Rot1)).T
+    rl_wheel = (rl_wheel.T.dot(Rot1)).T
+
+    outline[0, :] += x
+    outline[1, :] += y
+    fr_wheel[0, :] += x
+    fr_wheel[1, :] += y
+    rr_wheel[0, :] += x
+    rr_wheel[1, :] += y
+    fl_wheel[0, :] += x
+    fl_wheel[1, :] += y
+    rl_wheel[0, :] += x
+    rl_wheel[1, :] += y
+
+    plt.plot(np.array(outline[0, :]).flatten(),
+             np.array(outline[1, :]).flatten(), truckcolor)
+    plt.plot(np.array(fr_wheel[0, :]).flatten(),
+             np.array(fr_wheel[1, :]).flatten(), truckcolor)
+    plt.plot(np.array(rr_wheel[0, :]).flatten(),
+             np.array(rr_wheel[1, :]).flatten(), truckcolor)
+    plt.plot(np.array(fl_wheel[0, :]).flatten(),
+             np.array(fl_wheel[1, :]).flatten(), truckcolor)
+    plt.plot(np.array(rl_wheel[0, :]).flatten(),
+             np.array(rl_wheel[1, :]).flatten(), truckcolor)
+    plt.plot(x, y, "*")
+
+def traj_gen_and_track(ax,ay,target_speed):
+    """
+    
+    Parameters
+    ----------
+    ax: list[float]
+        a list of waypoint x coordinate
+    ay: list[float]   
+        a list of waypoint y coordinate 
+    target_speed: float
+        the target speed that we want to hit
+        
+    Returns
+    -------
+    t: list[float]
+        time sequence
+    x: list[float]
+    y: list[float]
+    yaw: list[float]
+        a series of x,y and yaw coordiante under lqr constraints
+    v: list[float]
+        a series of velocity under lqr constaints
+    acc: list[float]
+        a series of acceleration under lqr constaints
+    cx,cy: list[float]
+        a series of targeted waypoint generated by cubic spline
+    """
+    goal = [ax[-1], ay[-1]]
+
+    cx, cy, cyaw, ck, s = spline.calc_spline_course(
+        ax, ay, ds=0.1)
+    
+    sp = calc_speed_profile(cx, cy, cyaw, target_speed)
+
+    t, x, y, yaw, v, acc = closed_loop_prediction(cx, cy, cyaw, ck, sp, goal)
+    
+    return t, x, y, yaw, v, acc, cx, cy
+
+def main():
+    print("LQR steering control tracking start!!")
+    ax = [0.0, 6.0, 10.0,12.5, 12.5, 17.5, 20.0, 25.0]
+    ay = [0.0, 0.0, 0.0,0.0, 6.5, 6.5, 0.0, 0.0]
+    goal = [ax[-1], ay[-1]]
+
+    cx, cy, cyaw, ck, s = spline.calc_spline_course(
+        ax, ay, ds=0.1)
+    target_speed = 20.0 / 3.6  # simulation parameter km/h -> m/s
+
+    sp = calc_speed_profile(cx, cy, cyaw, target_speed)
+
+    t, x, y, yaw, v,acc = closed_loop_prediction(cx, cy, cyaw, ck, sp, goal)
+    
+    if show_animation:  # pragma: no cover
+        plt.close()
+        plt.subplots(1)
+        plt.plot(ax, ay, "xb", label="waypoints")
+        plt.plot(cx, cy, "-r", label="target course")
+        plt.plot(x, y, "-g", label="tracking")
+
+        # plt.grid(True)
+        plt.axis("equal")
+        plt.xlabel("x[m]")
+        plt.ylabel("y[m]")
+        for i,item in enumerate(x):
+            plot_car(x[i], y[i], yaw[i])
+        plt.legend()
+
+        plt.subplots(1)
+        plt.plot(s, [np.rad2deg(iyaw) for iyaw in cyaw], "-r", label="yaw")
+        plt.grid(True)
+        plt.legend()
+        plt.xlabel("line length[m]")
+        plt.ylabel("yaw angle[deg]")
+
+        plt.subplots(1)
+        plt.plot(s, ck, "-r", label="curvature")
+        plt.grid(True)
+        plt.legend()
+        plt.xlabel("line length[m]")
+        plt.ylabel("curvature [1/m]")
+
+        plt.show()
+
+
+if __name__ == '__main__':
+    # waypoints 
+    ax = [0.0, 5.0, 10.0,15, 15, 17.5, 20.0, 25.0]
+    ay = [0.0, 0.0, 0.0,0.0, 6.5, 6.5, 0.0, 0.0]
+    
+    ax = [0.0, 10.0, 10.0,0.0]
+    ay = [0.0, 0.0, 10.0,10.0]
+    
+    goal = [ax[-1], ay[-1]]
+
+    cx, cy, cyaw, ck, s = spline.calc_spline_course(
+        ax, ay, ds=0.1)
+    target_speed = 20.0 / 3.6  # simulation parameter km/h -> m/s
+
+    sp = calc_speed_profile(cx, cy, cyaw, target_speed)
+
+    t, x, y, yaw, v,acc = closed_loop_prediction(cx, cy, cyaw, ck, sp, goal)
+    
+    if show_animation:  # pragma: no cover
+        plt.close()
+        plt.subplots(1)
+        plt.plot(ax, ay, "xb", label="waypoints")
+        plt.plot(cx, cy, "-r", label="target course")
+        plt.plot(x, y, "-g", label="tracking")
+
+        # plt.grid(True)
+        plt.axis("equal")
+        plt.xlabel("x[m]")
+        plt.ylabel("y[m]")
+        # for i,item in enumerate(x):
+        #    plot_car(x[i], y[i], yaw[i])
+        plt.legend()
+
+        plt.subplots(1)
+        plt.plot(s, [np.rad2deg(iyaw) for iyaw in cyaw], "-r", label="yaw")
+        plt.grid(True)
+        plt.legend()
+        plt.xlabel("line length[m]")
+        plt.ylabel("yaw angle[deg]")
+
+        plt.subplots(1)
+        plt.plot(s, ck, "-r", label="curvature")
+        plt.grid(True)
+        plt.legend()
+        plt.xlabel("line length[m]")
+        plt.ylabel("curvature [1/m]")
+        
+        plt.subplots(1)
+        plt.plot(t, acc, "-r", label="curvature")
+        plt.grid(True)
+        plt.legend()
+        plt.xlabel("t [s]")
+        plt.ylabel("acceleration [m/s^2]")
+
+        plt.show()
+    
+
