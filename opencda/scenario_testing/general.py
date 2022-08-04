92cc405137733ddc174df027f23c86731b8a24b8
Zhaoliang <zhz03@g.ucla.edu>
2022-08-04 00:10:08 -0700

    Create a more general opencda interface

---
+++ opencda/scenario_testing/general.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+# Author: Runsheng Xu <rxx3386@ucla.edu>
+# License: TDG-Attribution-NonCommercial-NoDistrib
+import os
+
+import carla
+
+import opencda.scenario_testing.utils.sim_api as sim_api
+import opencda.scenario_testing.utils.customized_map_api as map_api
+from opencda.core.common.cav_world import CavWorld
+from opencda.scenario_testing.evaluations.evaluate_manager import \
+    EvaluationManager
+from opencda.scenario_testing.utils.yaml_utils import load_yaml
+
+
+def run_scenario(opt, config_yaml):
+    try:
+        scenario_params = load_yaml(config_yaml)
+
+        # create CAV world
+        cav_world = CavWorld(opt.apply_ml)
+
+        # basic_config
+        basic_config = scenario_params['basic_config']
+
+        if basic_config['xodr_path'] != 'None':
+            current_path = os.path.dirname(os.path.realpath(__file__))
+            xodr_path = os.path.join(
+                current_path,
+                basic_config['xodr_path'])
+        else:
+            xodr_path = None
+
+        if basic_config['Town'] != 'None':
+            town = basic_config['Town']
+        else:
+            town = None
+
+        # create scenario manager
+        scenario_manager = sim_api.ScenarioManager(scenario_params,
+                                                   opt.apply_ml,
+                                                   opt.version,
+                                                   xodr_path=xodr_path,
+                                                   town=town,
+                                                   cav_world=cav_world)
+
+        if opt.record:
+            scenario_manager.client. \
+                start_recorder(basic_config['log_name'], True)
+
+        spectator = scenario_manager.world.get_spectator()
+        spectator_config = basic_config['spectator_config']
+
+        if basic_config['type'] == 'single':
+            single_cav_list = \
+                scenario_manager.create_vehicle_manager(application=['single'])
+            spectator_vehicle = single_cav_list[0].vehicle
+
+        elif basic_config['type'] == 'platooning':
+            # create platoon members
+            platoon_list = \
+                scenario_manager.create_platoon_manager(
+                    map_helper=map_api.spawn_helper_2lanefree,
+                    data_dump=False)
+            spectator_vehicle = platoon_list[0].vehicle_manager_list[1].vehicle
+
+        # create background traffic in carla
+        traffic_manager, bg_veh_list = \
+            scenario_manager.create_traffic_carla()
+
+        # create evaluation manager
+        eval_manager = \
+            EvaluationManager(scenario_manager.cav_world,
+                              script_name=basic_config['script_name'],
+                              current_time=scenario_params['current_time'])
+
+        # run steps
+        while True:
+            scenario_manager.tick()
+            transform = spectator_vehicle.get_transform()
+            spectator.set_transform(carla.Transform(
+                transform.location +
+                carla.Location(
+                    z=spectator_config['z']),
+                carla.Rotation(
+                    pitch=-
+                    spectator_config['pitch'])))
+
+            if basic_config['type'] == 'single':
+                for i, single_cav in enumerate(single_cav_list):
+                    single_cav.update_info()
+                    control = single_cav.run_step()
+                    single_cav.vehicle.apply_control(control)
+
+            elif basic_config['type'] == 'platooning':
+
+                for platoon in platoon_list:
+                    platoon.update_information()
+                    platoon.run_step()
+
+                for i, single_cav in enumerate(single_cav_list):
+                    # this function should be added in wrapper
+                    if single_cav.v2x_manager.in_platoon():
+                        single_cav_list.pop(i)
+                    else:
+                        single_cav.update_info()
+                        control = single_cav.run_step()
+                        single_cav.vehicle.apply_control(control)
+
+    finally:
+        eval_manager.evaluate()
+
+        if opt.record:
+            scenario_manager.client.stop_recorder()
+
+        scenario_manager.close()
+
+        if basic_config['type'] == 'platooning':
+            for platoon in platoon_list:
+                platoon.destroy()
+        for v in single_cav_list:
+            v.destroy()
+        for v in bg_veh_list:
+            v.destroy()
+
---
+++ opencda_general.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Script to run different scenarios.
+"""
+
+# Author: Runsheng Xu <rxx3386@ucla.edu>
+# License: TDG-Attribution-NonCommercial-NoDistrib
+
+import argparse
+import importlib
+import os
+import sys
+
+from opencda.version import __version__
+
+
+def arg_parse():
+    parser = argparse.ArgumentParser(description="OpenCDA scenario runner.")
+    parser.add_argument('-t', "--test_scenario", required=True, type=str,
+                        help='Define the name of the scenario you want to test. The given name must'
+                             'match one of the testing scripts(e.g. single_2lanefree_carla) in '
+                             'opencda/scenario_testing/ folder'
+                             ' as well as the corresponding yaml file in opencda/scenario_testing/config_yaml.')
+    parser.add_argument("--record", action='store_true', help='whether to record and save the simulation process to'
+                                                              '.log file')
+    parser.add_argument("--apply_ml",
+                        action='store_true',
+                        help='whether ml/dl framework such as sklearn/pytorch is needed in the testing. '
+                             'Set it to true only when you have installed the pytorch/sklearn package.')
+    parser.add_argument('-v', "--version", type=str, default='0.9.11',
+                        help='Specify the CARLA simulator version, default'
+                             'is 0.9.11, 0.9.12 is also supported.')
+
+    opt = parser.parse_args()
+    return opt
+
+
+def main():
+    opt = arg_parse()
+    print("OpenCDA Version: %s" % __version__)
+
+    # testing_scenario = importlib.import_module("opencda.scenario_testing.%s" % opt.test_scenario)
+    testing_scenario = importlib.import_module("opencda.scenario_testing.general")
+
+    config_yaml = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                               'opencda/scenario_testing/config_yaml/%s.yaml' % opt.test_scenario)
+    if not os.path.isfile(config_yaml):
+        sys.exit("opencda/scenario_testing/config_yaml/%s.yaml not found!" % opt.test_cenario)
+
+    scenario_runner = getattr(testing_scenario, 'run_scenario')
+    # run scenario testing
+    scenario_runner(opt, config_yaml)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(' - Exited by user.')
