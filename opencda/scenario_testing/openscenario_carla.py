e713acd05ff4911b4389d82d7c41dccf124c7974
Dechen Gao <dcgao@ucdavis.edu>
2023-04-03 17:58:05 -0700

    Add VehicleManager

--- opencda/scenario_testing/openscenario_carla.py
+++ opencda/scenario_testing/openscenario_carla.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 # License: TDG-Attribution-NonCommercial-NoDistrib
 
-import math
 import carla
 import opencda.scenario_testing.utils.sim_api as sim_api
 from opencda.core.common.cav_world import CavWorld
 
 import time
 from multiprocessing import Process
+import psutil
 
 import scenario_runner as sr
+
 
 def exec_scenario_runner(scenario_params):
     """
@@ -22,120 +23,81 @@ def exec_scenario_runner(scenario_params):
     Returns
     -------
     """
-    scenario_runner = sr.ScenarioRunner(scenario_params.scenario)
+    scenario_runner = sr.ScenarioRunner(scenario_params.scenario_runner)
     scenario_runner.run()
     scenario_runner.destroy()
 
 
-def exec_ego_vehicle_runner(scenario_params):
-    """
-    Execute the ego vehicle runner
-
-    Parameters
-    ----------
-    scenario_params: Parameters of ScenarioRunner
-
-    Returns
-    -------
-    """
+def run_scenario(opt, scenario_params):
     scenario_runner = None
     cav_world = None
     scenario_manager = None
-    client = None
 
     try:
-        client_host = scenario_params.world.client_host
-        client_port = scenario_params.world.client_port
-        client = carla.Client(client_host, client_port)
-        world = client.get_world()
+        # Create CAV world
+        cav_world = CavWorld(opt.apply_ml)
+        # Create scenario manager
+        scenario_manager = sim_api.ScenarioManager(scenario_params,
+                                                   opt.apply_ml,
+                                                   opt.version,
+                                                   town=scenario_params.scenario_runner.town,
+                                                   cav_world=cav_world)
 
-        player = None
-        leading = None
+        # Create a background process to init and execute scenario runner
+        sr_process = Process(target=exec_scenario_runner,
+                             args=(scenario_params, ))
+        sr_process.start()
+        
+        world = scenario_manager.world
+        ego_vehicle = None
+        num_actors = 0
 
-        while player is None:
-            print("Waiting for the ego vehicle...")
+        while ego_vehicle is None or num_actors < scenario_params.scenario_runner.num_actors:
+            print("Waiting for the actors")
             time.sleep(2)
-            possible_vehicles = world.get_actors().filter('vehicle.*')
-            for vehicle in possible_vehicles:
+            vehicles = world.get_actors().filter('vehicle.*')
+            walkers = world.get_actors().filter('walker.*')
+            for vehicle in vehicles:
                 if vehicle.attributes['role_name'] == 'hero':
                     print("Ego vehicle found")
-                    player = vehicle
-                if vehicle.attributes['role_name'] == 'scenario':
-                    print("Leading vehicle found")
-                    leading = vehicle
-                
-        spectator = player.get_world().get_spectator()
+                    ego_vehicle = vehicle
+            num_actors = len(vehicles) + len(walkers)
+        print(f'Found all {num_actors} actors')
+
+        single_cav_list = scenario_manager.create_vehicle_manager_from_scenario_runner(
+            vehicle=ego_vehicle,
+        )
+
+        spectator = ego_vehicle.get_world().get_spectator()
         # Bird view following
-        spectator_altitude = 80
+        spectator_altitude = 50
         spectator_bird_pitch = -90
-        
+
         while True:
-            world.tick()
-            
-            # Get the control of the ego vehicle
-            control = leading.get_control()
-            # Get the distance between the ego and leading vehicles
-            distance = leading.get_location().distance(player.get_location())
-            # Get the velocity of the ego vehicle on the x-y plane
-            ego_velocity = math.sqrt(player.get_velocity().x ** 2 + player.get_velocity().y ** 2)
-            leading_velocity = math.sqrt(leading.get_velocity().x ** 2 + leading.get_velocity().y ** 2)
-            
-            # Wait for the leading vehicle to start
-            if leading_velocity < 0.1: 
-                control = carla.VehicleControl(throttle=0.50000, steer=0, brake=0.000000, hand_brake=False,
-                                            reverse=False, manual_gear_shift=False, gear=0)
-            # If the leading vehicle is too close, stop the ego vehicle
-            elif distance < 1.7 * ego_velocity or distance < 10:
-                control = carla.VehicleControl(throttle=0, steer=0, brake=1.0, hand_brake=True,
-                                            reverse=False, manual_gear_shift=False, gear=0)
-            
-            print("Distance: {}, Ego velocity: {}, Leading velocity: {}".format(distance, ego_velocity, leading_velocity))
+            scenario_manager.tick()
+            ego_cav = single_cav_list[0].vehicle
+
             # Bird view following
             view_transform = carla.Transform()
-            view_transform.location = player.get_transform().location
+            view_transform.location = ego_cav.get_transform().location
             view_transform.location.z = view_transform.location.z + spectator_altitude
             view_transform.rotation.pitch = spectator_bird_pitch
             spectator.set_transform(view_transform)
 
             # Apply the control to the ego vehicle
-            player.apply_control(control)
-            time.sleep(0.05)
+            for _, single_cav in enumerate(single_cav_list):
+                single_cav.update_info()
+                control = single_cav.run_step()
+                single_cav.vehicle.apply_control(control)
+            time.sleep(0.01)
+
     finally:
         if cav_world is not None:
             cav_world.destroy()
         print("Destroyed cav_world")
         if scenario_manager is not None:
-            scenario_manager.destroyActors()
             scenario_manager.close()
+        print("Destroyed scenario_manager")
         if scenario_runner is not None:
             scenario_runner.destroy()
-            del scenario_runner
-        print("Destroyed scenario_runner...")
-
-
-def run_scenario(opt, scenario_params):
-    try:
-        # Create CAV world
-        cav_world = CavWorld(opt.apply_ml)
-        # Create scenario manager
-        sim_api.ScenarioManager(scenario_params,
-                                opt.apply_ml,
-                                opt.version,
-                                town=scenario_params.scenario.town,
-                                cav_world=cav_world)
-
-        # Create a background process to init and execute scenario runner
-        sr_process = Process(target=exec_scenario_runner, args=(scenario_params, ))
-        # Create a parallel process to init and run EI-Drive
-        ei_process = Process(target=exec_ego_vehicle_runner, args=(scenario_params, ))
-
-        sr_process.start()
-        ei_process.start()
-        
-        sr_process.join()
-        ei_process.join()
-        
-
-
-    except Exception as e:
-        print("Error: {}".format(e))
+        print("Destroyed scenario_runner")
