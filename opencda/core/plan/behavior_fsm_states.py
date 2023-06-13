a163dc15b794957df40e8dc5f403109b4c1b9a03
xuhan <hanxu417@g.ucla.edu>
2023-06-13 13:49:03 -0700

    add behavior fsm states for behavior planning

---
+++ opencda/core/plan/behavior_fsm_states.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+"""Finite State Machine
+"""
+
+# Author: Xu Han <>
+# License: TDG-Attribution-NonCommercial-NoDistrib
+
+from enum import Enum
+
+
+class BehaviorSuperStates(Enum):
+    """
+    The finite state machine superstates for generic behavioral planning.
+    These classes are used to indicate the current status
+    of the CAV during platooning.
+
+    Attributes
+    ----------
+    SEARCHING : int
+        The vehicle is not in any platoon and currently searching one to join.
+    """
+    LANE_FOLLOWING = 0
+    INTERSECTION = 1
+    OVERTAKING = 2
+    COLLISION_AVOIDANCE = 3
+
+class BehaviorStates(Enum):
+    """
+    The finite state machine states for generic behavioral planning.
+    These classes are used to indicate the current status
+    of the CAV during platooning.
+
+    Attributes
+    ----------
+    SEARCHING : int
+        The vehicle is not in any platoon and currently searching one to join.
+    """
+    # LANE_FOLLOWING
+    GO_STRAIGHT = 0
+    PREPARE_LANE_CHANGE_LEFT = 1
+    PREPARE_LANE_CHANGE_RIGHT = 2
+    LANE_CHANGE_LEFT = 3
+    LANE_CHANGE_RIGHT = 4
+    CAR_FOLLOWING = 5
+
+    # INTERSECTION
+    # GO_STRAIGHT = 0
+    # CAR_FOLLOWING = 5
+    TURN_LEFT = 6
+    TURN_RIGHT = 7
+    STOP = 8
+
+    # OVERTAKING
+    # GO_STRAIGHT = 0
+    # PREPARE_LANE_CHANGE_LEFT = 1
+    # PREPARE_LANE_CHANGE_RIGHT = 2
+    # LANE_CHANGE_LEFT = 3
+    # LANE_CHANGE_RIGHT = 4
+
+    # COLLISION_AVOIDANCE
+    SLOW_DOWN = 9
+    EMERGENCY_STOP = 10
+    EMERGENCY_LANE_CHANGE_LEFT = 11
+    EMERGENCY_LANE_CHANGE_RIGHT = 12
